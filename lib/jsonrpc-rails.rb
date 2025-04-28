35b76f889ab8864b93bcba40432a3ca36675bce0
Abdelkader Boudih <terminale@gmail.com>
2025-04-28 12:26:02 +0100

    chore: ignore Gemfile.lock

--- lib/jsonrpc-rails.rb
+++ lib/jsonrpc-rails.rb
@@ -7,7 +7,5 @@ require_relative "json_rpc/request"
 require_relative "json_rpc/response"
 require_relative "json_rpc/notification"
 
-# Define the top-level module for the gem (optional, but good practice)
 module JSONRPC_Rails
-  # You might add gem-level configuration or methods here if needed later.
 end
